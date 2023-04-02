98b12451511d86b24514f552a55bb6d20cc0f6b0
Karthikeyan <karthikeyanfz1111@gmail.com>
2023-04-02 18:59:26 +0530

    login modification 2-4-2023 7:00pm

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+
+
+// spam is used to give error note if any one input is invalid check with user[database]
+
+
+function switchVisible() {
+  const x=document.getElementById('loginModule');
+  const y=document.getElementById('forgotpassModule');
+  if(x){
+    if (x.style.display == 'none') {
+      x.style.display = 'block';
+      y.style.display = 'none';
+    } else {
+      y.style.display = 'Block';
+      x.style.display = 'none';
+    }
+  }
+}
+
+var login=document.getElementById('login');
+login.addEventListener("click",function()
+{
+    window.open("./main.html", "_self");
+});
